2559769142aa19898f2e8ec7c5893061600e2f64
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-11-30 13:05:31 +0100

    error to check - AppDescription import does not work

--- app/script.js
+++ app/script.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import AppDescription from './AppDescription';
 
 class App extends React.Component {
 
@@ -7,8 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
+        <AppDescription />
         <img src="./images/work.png" />
         <img src="./images/rest.png" />
         <div className="timer">
