7a35c47bc32537c7540e28d367b25afc5099bfaa
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-11-30 16:48:02 +0100

    remove frame and finish app

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ function main() {
   // create new window
   let mainWindow = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
-    width: 800,
-    height: 600,
+    width: 520,
+    height: 650,
+    frame: false,
   })
 
   // load app/index.html as the window content
   mainWindow.loadFile(path.join('app', 'index.html'));
 
   // add DevTools
-  mainWindow.webContents.openDevTools();
+  ///mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', main);
